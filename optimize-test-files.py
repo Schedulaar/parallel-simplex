8d35be0ce0e6a391c56485b5e425e123e4090eed
Michael Markl <marklmichael98@gmail.com>
2020-12-09 11:53:09 +0100

    Optimize with scipy

--- optimize-test-files.py
+++ optimize-test-files.py
@@ -1,5 +1,6 @@
 from scipy.optimize import linprog
 from numpy import genfromtxt
+import time
 
 n = 1000
 
@@ -7,5 +8,11 @@ A = genfromtxt(str(n) + '-A.csv', delimiter=',')[:n, :n]
 b = genfromtxt(str(n) + '-b.csv', delimiter='\n')
 c = genfromtxt(str(n) + '-c.csv', delimiter='\n')
 
+start = time.time()
+
 res = linprog(-c, A, b, bounds=(0, None))
-print(res)
+
+end = time.time()
+print(end - start)
+
+print(res.fun)
