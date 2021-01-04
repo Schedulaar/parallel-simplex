e381572681dab344b6e5d6a27b8467b53e9e6bc2
Michael Markl <marklmichael98@gmail.com>
2021-01-04 03:17:41 +0100

    Generate test files from mps

---
+++ generate-test-files-from-mps.py
@@ -0,0 +1,68 @@
+import smps_loader as loader
+
+
+def truncate(f, n=6):
+    """Truncates/pads a float f to n decimal places without rounding"""
+    s = '{:f}'.format(f)
+    s.strip("0")
+    i, p, d = s.partition('.')
+    return '.'.join([i, d.rstrip("0")])
+
+
+result = loader.load_mps("25FV47.SIF")
+
+(name, objective_name, row_names, col_names, _, types, c, A, rhs_names, rhs, bnd_names, bnd) = result
+
+if len(rhs_names) != 1:
+    raise Exception("Can't handle more than one b vector")
+
+b = rhs[rhs_names[0]]
+
+fA = open(name + "-A.csv", "w")
+fb = open(name + "-b.csv", "w")
+fc = open(name + "-c.csv", "w")
+fshape = open(name + "-shape.csv", "w")
+
+(m, n) = A.shape
+actualM = m
+
+for i in range(m):
+    if types[i] == "L":
+        for j in range(n):
+            fA.write(truncate(A[i, j]) + ",")
+        fb.write(truncate(b[i]))
+        if i < m - 1:
+            fA.write("\n")
+            fb.write("\n")
+    if types[i] == "G":
+        for j in range(n):
+            fA.write(truncate(-A[i, j]) + ",")
+        fb.write(truncate(-b[i]))
+        if i < m - 1:
+            fA.write("\n")
+            fb.write("\n")
+    if types[i] == "E":
+        for j in range(n):
+            fA.write(truncate(A[i, j]) + ",")
+        fb.write(truncate(b[i]))
+        fA.write("\n")
+        fb.write("\n")
+        for j in range(n):
+            fA.write(truncate(-A[i, j]) + ",")
+        fb.write(truncate(-b[i]))
+        if i < m - 1:
+            fA.write("\n")
+            fb.write("\n")
+        actualM += 1
+
+fshape.write('{}\n{}'.format(actualM, n))
+fshape.close()
+
+for j in range(n):
+    fc.write(truncate(-c[j]))
+    if j < n - 1:
+        fc.write("\n")
+
+fA.close()
+fb.close()
+fc.close()
